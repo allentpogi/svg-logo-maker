edefa1178e714945611ae0fd21bc5886ecfe4aaf
allen <allen.tuazon@gmail.com>
2023-04-29 23:02:22 +1000

    completed all the classes. the index.js is more or less completed with the checking of the shape and generating the logo. also saved example logos in the example folder

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// Packages needed for this application
+// Dependencies needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./lib/square');
+const SquareLogo = require('./lib/square');
+const CircleLogo = require('./lib/circle');
+const EllipseLogo = require('./lib/ellipse');
+const TriangleLogo = require('./lib/triangle');
 
 // Array of questions for user input
 const questions = [
@@ -26,7 +29,7 @@ const questions = [
       type: 'list',
       name: 'shape',
       message: 'Choose a shape for your logo.',
-      choices: ['square', 'circle', 'ellipse'],
+      choices: ['square', 'circle', 'ellipse', 'triangle'],
     },
     {
       type: 'input',
@@ -43,72 +46,49 @@ const writeTofile = (svgTemplate) => {
     );
 };
 
-
-// Function to initialize app
-
-// function init() {
-//     inquirer
-//         .prompt(questions)
-//         .then((answers) => {
-//             let svgTemplate
-
-//             if (answers.shape === 'square') {
-//                 svgTemplate =
-// `
-// <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-// <rect x="75" y="25" rx="20" ry="20" width="160" height="160" fill="${answers.shapeColour}" />
-// <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColour}">${answers.text}</text>
-// </svg>
-
-// `;
-//             } else if (answers.shape === 'ellipse') {
-//                 svgTemplate =
-// `
-// <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-// <ellipse cx="150" cy="100" rx="100" ry="50" fill="${answers.shapeColour}" />
-// <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColour}">${answers.text}</text>
-// </svg>
-
-// `;
-
-//             } else {
-//                 svgTemplate =
-// `
-// <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-// <circle cx="150" cy="100" r="80" fill="${answers.shapeColour}" />
-// <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColour}">${answers.text}</text>
-// </svg>
-
-// `;
-//             };
-
-//             writeTofile(svgTemplate);
-
-//         });
-// };
-
-
+// Function to initialise the application. This will start the inquirer application and ask questions to users.
 function init() {
 
     inquirer
         .prompt(questions)
         .then((answers) => {
+
+            // Declare variable for the contents of the svg file
+            let svgTemplate
             
-            // if (answers.shape === 'square') {
-                const newSquareLogo = new shapes (
+
+            // Check what shape the user selected and then create the object using the applicable class
+            if (answers.shape === 'square') {
+                const newSquareLogo = new SquareLogo (
                     answers.text,
                     answers.textColour,
                     answers.shapeColour
                 );
-            // }
+                svgTemplate = newSquareLogo.generateSquarelogo()
+            } else if (answers.shape === 'circle') {
+                const newCirclelogo = new CircleLogo (
+                    answers.text,
+                    answers.textColour,
+                    answers.shapeColour
+                );
+                svgTemplate = newCirclelogo.generateCirclelogo()
+            } else if (answers.shape === 'ellipse') {
+                const newEllipselogo = new EllipseLogo (
+                    answers.text,
+                    answers.textColour,
+                    answers.shapeColour
+                );
+                svgTemplate = newEllipselogo.generateEllipselogo()
+            } else {
+                const newTrianglelogo = new TriangleLogo (
+                    answers.text,
+                    answers.textColour,
+                    answers.shapeColour
+                );
+                svgTemplate = newTrianglelogo.generateTrianglelogo()
+            }
 
-            console.log(newSquareLogo);
-            
-            const svgTemplate = newSquareLogo.generateSquarelogo()
-
-            
-            console.log(svgTemplate);
-
+            // Call the function to write the template into the file
             writeTofile(svgTemplate);
 
         });
